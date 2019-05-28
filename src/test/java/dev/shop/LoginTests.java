c71f57a84d8d5842e8f1f5ad26ce2ccc32baa544
Ksenia Yarysh <ksana32@gmail.com>
2019-05-28 11:55:16 +0500

    initial commit

---
+++ src/test/java/dev/shop/LoginTests.java
@@ -0,0 +1,66 @@
+package dev.shop;
+
+import dev.shop.models.SignInPage;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(value = Parameterized.class)
+public class LoginTests {
+    private WebDriver webDriver;
+    private WebDriverWait wait;
+    private String password;
+    private String result;
+
+    public LoginTests(String password, String result) {
+        this.password = password;
+        this.result = result;
+    }
+
+    @Before
+    public void setUp(){
+        webDriver = new ChromeDriver();
+        webDriver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        webDriver.manage().window().maximize();
+        wait = new WebDriverWait(webDriver, 30);
+    }
+
+
+    @Parameters
+    public static Collection<Object[]> data() {
+        Collection<Object[]> data = new ArrayList<>();
+        data.add(new Object[]{"bQp0Lem1Zv", "companies"});
+        data.add(new Object[]{"bQp0Lem1xxxxZv", "sign-in"});
+        return data;
+    }
+
+    @Test
+    public void testLogin(){
+        webDriver.get("https://dev-shop.jowi.club/auth/sign-in");
+        SignInPage page = new SignInPage(webDriver);
+        page.enterLogin("79630830696");
+        page.enterPhone(password);
+        page.pressSubmit();
+        wait.until(ExpectedConditions.urlContains(result));
+        assertTrue(webDriver.getCurrentUrl().contains(result));
+    }
+
+    @After
+    public void tearDown(){
+        webDriver.quit();
+    }
+}
---
+++ src/test/java/dev/shop/models/SignInPage.java
@@ -0,0 +1,30 @@
+package dev.shop.models;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class SignInPage {
+    private WebDriver webDriver;
+
+    public SignInPage(WebDriver webDriver) {
+        this.webDriver = webDriver;
+    }
+
+    public void enterLogin(String login){
+        webDriver.findElement(By.cssSelector ("[name=login]")).sendKeys(login);
+    }
+
+    public void enterPhone(String password){
+        webDriver.findElement(By.cssSelector ("[name=password]")).sendKeys(password);
+    }
+
+    public void pressSubmit(){
+          WebElement we = new WebDriverWait(webDriver, 60).
+                  until(ExpectedConditions.elementToBeClickable(By.cssSelector ("[type=submit]")));
+          we.click();
+    }
+// )  presenceOfElementLocated(
+}
